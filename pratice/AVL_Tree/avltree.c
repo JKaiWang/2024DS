4318cab61a86f8da342e28530522d86cb8375210
JKaiWang <a0988347904@gmail.com>
2024-12-11 02:14:07 +0800

    finish all the avl tree

--- pratice/AVL_Tree/avltree.c
+++ pratice/AVL_Tree/avltree.c
@@ -82,6 +82,66 @@ Node* insert(Node* root , int data){
     return root;
 }
 
+Node* minValueNode(Node* node){
+    Node* current = node;
+    while(current->left != NULL){
+        current = current->left;
+    }
+    return current;
+}
+
+Node* deleteNode(Node* root, int key){
+    if(root == NULL) return root;
+    if(key < root->val){
+        root->left = deleteNode(root->left , key);
+    }
+    else if(key > root->val){
+        root->right = deleteNode(root->right , key);
+    }
+    else{
+        if((root->left == NULL) || (root->right == NULL)){
+            Node *temp = root->left ? root->left : root->right;
+            // No Child case
+            if(temp == NULL){
+                temp =root ; 
+                root = NULL;
+            }   
+            // One Child case
+            else{
+                *root = *temp ;  
+            }
+            free(temp);
+        }
+        else{
+            Node* temp = minValueNode(root->right);
+            root->val = temp->val;
+            root->right = deleteNode(root->right , temp->val);
+        }
+
+    }
+    if(root == NULL){
+        return root;
+    }
+    root->height = 1+ mac(height(root->left) , height(root->right));
+    int balance = BalanceFactor(root);
+
+    if(balance > 1 && BalanceFactor(root->left) >= 0){
+        return rightRotate(root);
+    }
+    if(balance > 1 && BalanceFactor(root->left) < 0){
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+    if(balance < -1 && BalanceFactor(root->right) <= 0){
+        return leftRotate(root);
+    }
+    if(balance < -1 && BalanceFactor(root->right) > 0){
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+    return root;
+}
+
 void inOrder(Node* root){
     inOrder(root->left);
     printf("%d " , root->val );
