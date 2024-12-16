cfe1c4261445c29ce98d8b21c6935c6e3c2d5776
JKaiWang <a0988347904@gmail.com>
2024-12-16 22:07:57 +0800

    done with the binomial Heap

--- homework/HW2/p1.c
+++ homework/HW2/p1.c
@@ -1,5 +1,94 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct HashTable{
-    int 
-}HashTable;
+
+
+int** hashtable;
+int bucket;
+int slot;
+
+void create(){
+    hashtable = (int**)malloc(bucket*sizeof(int*));
+    for(int i=0 ; i< bucket ; i++){
+        hashtable[i] = (int*)malloc(slot*sizeof(int));
+        for(int j=0 ; j< slot ; j++){
+            hashtable[i][j] = -1;
+        }
+    }   
+}
+
+void insert(int key){
+    int temp = key % bucket;
+    for(int i=0 ; i<bucket ; i++){
+        int current = (temp+i)%bucket;
+        for(int i=0 ; i<slot ; i++){
+            if(hashtable[current][i] == -1){
+                hashtable[current][i] = key;
+                return;
+            }
+        }
+    }
+}
+
+void search(int key){
+    int temp =key % bucket;
+    for(int i=0; i < bucket-temp ; i++){
+        int current = (temp+i)%bucket;
+        for(int i=0 ; i< slot ; i++){
+            if(hashtable[current][i] == key){
+                printf("%d %d\n" , current , i);
+                return;
+            }
+        }
+    }
+}
+
+void delete(int key){
+    int temp = key % bucket;
+    for(int i=0 ; i<bucket-temp ; i++){
+        int current = (temp + i) % bucket;
+        for(int i=0 ; i<slot ; i++){
+            if(hashtable[current][i] == key){
+                hashtable[current][i] = -1;
+                return;
+            }
+        }
+        temp +=1;
+    }
+ㄨㄨ
+
+int main(){
+    char string[100];
+    int data;
+    
+    //printf("yes");
+    while(scanf("%s " , string)==1){
+        if(string[0] == 'b'){
+            scanf("%d" , &bucket);
+            //printf("bucket\n");
+        }
+        if(string[0] == 's' && string[1] == 'l'){
+            scanf("%d" , &slot);
+            create();
+            //printf("slot\n");
+        }
+        if(string[0] == 'i'){
+            scanf("%d" , &data);
+            insert(data);
+            //printf("insert %d \n" , data);
+        }
+        if(string[0] == 's' && string[1] == 'e'){
+            scanf("%d" , &data);
+            search(data);
+            //printf("search\n");
+            
+        }
+        if(string[0] == 'd'){
+            scanf("%d" , &data);
+            delete(data);
+            //printf("delete\n");
+        }
+        if(string[0] == 'e'){
+            break;
+        }
+    }
+}
---
+++ homework/HW2/p2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void insert(int key){
+
+}
+
+void delete(int key){
+
+}
+
+void decrease(int key  , int value){
+
+}
+
+void extractMin(){
+
+}
+
+int main(){
+    char string[100];
+    int key;
+    int value;
+    while(scanf("%s" , string) == 1){
+        if(string[0] == 'i'){
+            scanf("%d" , &key);
+        }
+        if(string[0] == 'd' && string[2] == 'l'){
+        }
+        if(string[0] == 'd' && string[2] == 'c'){
+
+        }
+        if(string[0] == 'e'  && string[2] == 't'){
+
+        }
+        if(string[0] == 'e' && string[2] == 'i'){
+
+        }
+    }
+}
